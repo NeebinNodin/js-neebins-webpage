1df7ab5ef350097020216369149ac9ff7e4752c9
SummerEcho <164468785+NeebinNodin@users.noreply.github.com>
2025-01-26 00:09:50 -0600

    done with demo

--- site.js
+++ site.js
@@ -1,2 +1,28 @@
-const mainContentSection = document.querySelector('h2')
-mainContentSection.style.color = 'red'
+/* const mainContentSection = document.querySelector('h2')
+mainContentSection.style.color = 'red' 
+const link = document.querySelector('#fork')
+link.style.textDecoration = 'underline'*/
+/* const section = document.querySelector('section')
+
+const active = section.querySelector('section .active')
+active.style.color = 'blue'
+
+const p = section.querySelector('p')
+
+const span = section.querySelector('span') */
+//const activeElements = document.querySelectorAll('.active')
+
+// add bordor
+//activeElements.forEach(el => el.style.border = '1px solid red')
+
+//for (let i = 0; i < activeElements.length; i++){
+//    activeElements[i].style.border = '1px solid red'
+//}
+
+document.body.style.background  = 'lightgrey'
+document.title = "Neebin's Website"
+
+const contact = document.querySelector('#contact')
+contact.addEventListener('mouseover', () => {
+    contact.closest('nav').style.backgroundColor = 'lightblue'
+})
